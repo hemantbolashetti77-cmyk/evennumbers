5df4679a986fa1e7e7eaf989f0a5bbae860fb497
hemant17 <hemantbolashetti77@gamail.com>
2025-10-23 16:46:49 +0530

    save

--- even.py
+++ even.py
@@ -2,12 +2,22 @@ numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
 
 even_count = 0
 odd_count = 0
+prime_count = 0
 
 for n in numbers:
     if n % 2 == 0:
         even_count += 1
     else:
         odd_count += 1
+    if n > 1:
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                break
+        else:
+            prime_count += 1
+
 
 print("Even numbers:", even_count)
 print("Odd numbers:", odd_count)
+print("Prime numbers:", prime_count)
+
